48ea1b88d4f8e571b00754319afb3c1edb3d04b8
hitenjadeja <hitenjadeja@gmail.com>
2021-11-12 11:18:45 +0000

    Add logout url

--- azure_ad_auth/urls.py
+++ azure_ad_auth/urls.py
@@ -1,8 +1,9 @@
-from .views import auth, complete
+from .views import auth, complete, logout
 from django.conf.urls import url
 
 
 urlpatterns = [
     url(r'^login/$', auth, name='azure_login'),
+    url(r'^logout/$', logout, name='azure_logout'),
     url(r'^complete/$', complete, name='azure_complete'),
 ]
